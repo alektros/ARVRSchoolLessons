272db5c57a019cb77e6cfc676f1a398a03322b30
Alex <otravnick@gmail.com>
2018-11-07 10:04:39 +0300

    Add Lesson 03
    Key points:
    - Working with Vuforia (importing, VuforiaConfiguration)
    - Import custom Vuforia Database
    - Setup ImageTarget (size, extended tracking)
    - Development of custom TrackableEventHandler
    - Splitting tracking system and content with MarkerController

---
+++ Assets/ARVRSchool/Lesson 03/CustomTrackableEventHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+namespace Ru.Funreality.ARVRLessons.Lesson03
+{
+    public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+    {
+        private TrackableBehaviour _trackableBehaviour;
+
+        public Action<CustomTrackableEventHandler> OnMarkerFound = delegate(CustomTrackableEventHandler handler) { };
+        public Action<CustomTrackableEventHandler> OnMarkerLost = delegate(CustomTrackableEventHandler handler) { };
+
+
+        protected virtual void Start()
+        {
+            _trackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (_trackableBehaviour)
+                _trackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (_trackableBehaviour)
+                _trackableBehaviour.UnregisterTrackableEventHandler(this);
+        }
+
+        public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                OnMarkerFound(this);
+            }
+            else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                     newStatus == TrackableBehaviour.Status.NO_POSE)
+            {
+                OnMarkerLost(this);
+            }
+            else
+            {
+                OnMarkerLost(this);
+            }
+        }
+    }
+}
---
+++ Assets/ARVRSchool/Lesson 03/MarkerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ru.Funreality.ARVRLessons.Lesson03
+{
+    public class MarkerController : MonoBehaviour
+    {
+        [SerializeField] private ContentData[] _data;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            foreach (var handler in _data)
+            {
+                handler.Handler.OnMarkerFound += OnMarkerFound;
+                handler.Handler.OnMarkerLost += OnMarkerLost;
+            }
+        }
+
+
+        private void OnMarkerLost(CustomTrackableEventHandler handler)
+        {
+            Debug.LogWarning("OnMarkerLost " + handler.name);
+        }
+
+        private void OnMarkerFound(CustomTrackableEventHandler handler)
+        {
+            Debug.LogWarning("OnMarkerFound " + handler.gameObject.name);
+        }
+
+        private void Update()
+        {
+            foreach (var handler in _data)
+            {
+                handler.Update();
+            }
+        }
+
+        [Serializable]
+        public struct ContentData
+        {
+            public CustomTrackableEventHandler Handler;
+            public GameObject Content;
+
+            public void Update()
+            {
+                Content.transform.position = Handler.transform.position;
+                Content.transform.rotation = Handler.transform.rotation;
+            }
+        }
+    }
+}
