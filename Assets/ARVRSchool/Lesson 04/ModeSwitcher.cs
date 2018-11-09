6e1d96cc742995611d77cd3ce3651d7b050eabe6
alex <otravnick@gmail.com>
2018-11-09 11:06:12 +0300

    Refactor ModeSwitcher.cs

--- Assets/ARVRSchool/Lesson 04/ModeSwitcher.cs
+++ Assets/ARVRSchool/Lesson 04/ModeSwitcher.cs
@@ -1,87 +1,77 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using Vuforia;
 
-public class ModeSwitcher : MonoBehaviour
+namespace Ru.Funreality.ARVRLessons.Lesson04
 {
+    public class ModeSwitcher : MonoBehaviour
+    {
+        [SerializeField] private Camera           _arCamera;
+        [SerializeField] private Camera           _camera3D;
+        [SerializeField] private Camera           _vrCamera;
+        [SerializeField] private VuforiaBehaviour _vuforiaBehaviour;
 
-	public Camera ARCamera;
+        public void SwitchTo3D()
+        {
+            StartCoroutine(LoadDevice("None"));
+            DisableCameras();
+            _camera3D.gameObject.SetActive(true);
+        }
 
-	public VuforiaBehaviour _VuforiaBehaviour;
-	
-	public Camera Camera3D;
+        public void SwitchToAR()
+        {
+            DisableCameras();
+            _arCamera.gameObject.SetActive(true);
+            InitializeVuforia();
+        }
 
-	public Camera VRCamera;
-	
-	public void SwitchTo3D()
-	{
-		StartCoroutine(LoadDevice("None"));
-		DisableCameras();
-		Camera3D.gameObject.SetActive(true);
-		
-	}
+        public void SwitchToVR()
+        {
+            DisableCameras();
+            InitializeGVR();
+            _vrCamera.gameObject.SetActive(true);
+        }
 
-	public void SwitchToAR()
-	{
-		DisableCameras();
-		ARCamera.gameObject.SetActive(true);
-		InitializeVuforia();
-	}
+        private void Awake()
+        {
+            SwitchTo3D();
+        }
 
+        private void DisableCameras()
+        {
+            _arCamera.gameObject.SetActive(false);
+            _vrCamera.gameObject.SetActive(false);
+            _camera3D.gameObject.SetActive(false);
+        }
 
-	public void SwitchToVR()
-	{
-		DisableCameras();
-		InitializeGVR();
-		VRCamera.gameObject.SetActive(true);
-	}
+        private void InitializeVuforia()
+        {
+            StartCoroutine(InitVuforiaRoutine());
+        }
 
-	private void Awake()
-	{
-		
-		SwitchTo3D();
-	}
+        private void InitializeGVR()
+        {
+            StartCoroutine(LoadDevice("Cardboard"));
+        }
 
-	private void DisableCameras()
-	{
-		ARCamera.gameObject.SetActive(false);
-		VRCamera.gameObject.SetActive(false);
-		Camera3D.gameObject.SetActive(false);
-	}
+        private IEnumerator LoadDevice(string newDevice)
+        {
+            XRSettings.LoadDeviceByName(newDevice);
+            yield return null;
+            XRSettings.enabled = true;
+        }
 
+        private IEnumerator InitVuforiaRoutine()
+        {
+            VuforiaRuntime.Instance.InitVuforia();
+            while (VuforiaRuntime.Instance.InitializationState != VuforiaRuntime.InitState.INITIALIZED)
+            {
+                Debug.Log(VuforiaRuntime.Instance.InitializationState);
+                yield return new WaitForEndOfFrame();
+            }
 
-	private void InitializeVuforia()
-	{
-		StartCoroutine(InitVuforiaRoutine());
-	}
-
-
-	private void InitializeGVR()
-	{
-		StartCoroutine(LoadDevice("Cardboard"));
-
-
-	}
-	
-	IEnumerator LoadDevice(string newDevice)
-	{
-		XRSettings.LoadDeviceByName(newDevice);
-		yield return null;
-		XRSettings.enabled = true;
-	}
-
-	private IEnumerator InitVuforiaRoutine()
-	{
-		VuforiaRuntime.Instance.InitVuforia();
-		while (VuforiaRuntime.Instance.InitializationState != VuforiaRuntime.InitState.INITIALIZED)
-		{
-			Debug.Log(VuforiaRuntime.Instance.InitializationState);
-			yield return new WaitForEndOfFrame();
-		}
-
-		_VuforiaBehaviour.enabled = true;
-	}
-	
+            _vuforiaBehaviour.enabled = true;
+        }
+    }
 }
