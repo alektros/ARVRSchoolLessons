8582f113c59ec201279b4c02f09bc9f74876d30f
Alex <otravnick@gmail.com>
2018-12-07 10:32:07 +0300

    Develop stub

---
+++ Assets/ARVRSchool/Lesson 08/Animations/CubeAnimationController.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class CubeAnimationController : MonoBehaviour
+{
+
+	public Color TestColor;
+	
+	public Animator animator;
+
+	public void Jump()
+	{
+		animator.SetTrigger("Jumping");
+	}
+
+	public void OnCustomPropertyAnimFinish(int param)
+	{
+		Debug.Log("OnCustomPropertyAnimFinish " + param);
+	}
+
+	private void Start()
+	{
+
+		var prefab = Resources.Load("Tower");
+		Instantiate(prefab);
+	}
+}
