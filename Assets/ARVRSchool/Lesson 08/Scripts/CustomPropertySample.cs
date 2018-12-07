ed7d9282bd05473d70aa1702e848bb00634e1cdd
alex <otravnick@gmail.com>
2018-12-07 12:06:54 +0400

    Develop

---
+++ Assets/ARVRSchool/Lesson 08/Scripts/CustomPropertySample.cs
@@ -0,0 +1,18 @@
+﻿namespace Ru.Funreality.ARVRLessons.Lesson08
+{
+    using UnityEngine;
+
+    public class CustomPropertySample : MonoBehaviour
+    {
+        public  Color    TestColor;
+        private Material _targetMaterial;
+        public  void     OnCustomPropertyAnimFinish(int param) { Debug.Log("OnCustomPropertyAnimFinish " + param); }
+
+        private void Start()
+        {
+            _targetMaterial = GetComponent<MeshRenderer>().material;
+        }
+
+        private void Update() { _targetMaterial.SetColor("_Color", TestColor); }
+    }
+}
---
+++ Assets/ARVRSchool/Lesson 08/Scripts/MechanimSample.cs
@@ -0,0 +1,11 @@
+﻿namespace Ru.Funreality.ARVRLessons.Lesson08
+{
+    using UnityEngine;
+
+    public class MechanimSample : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        public                   void     Jump()  { _animator.SetTrigger("Jumping"); }
+        public                   void     Twist() { _animator.SetTrigger("Twist"); }
+    }
+}
